f6cc8305b01cf6abbbc4d2bcd1063dbfefb5a3c0
Patrick Han <patrick-fullstack@users.noreply.github.com>
2017-08-10 17:03:16 +0800

    adding test case
    adding test case

--- test/test.js
+++ test/test.js
@@ -4,46 +4,61 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj)
           done()
         }, 0)
       }
     }
     obj.say()
-  }) 
+  })
 
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      this.should.equal(global)
     }
     test()
   })
 
-  describe('bind', function () {
-    it('bind undefined', function () {
-      var obj = {
-        say: function () {
-          function _say() {
-            // this 是什么？想想为什么？
-            this.should.equal(null)
-          }
-          return _say.bind(obj)
-        }()
-      }
-      obj.say()
-    })
-
-    it('bind normal', function () {
-      var obj = {}
-      obj.say = function () {
+  it('bind undefined', function () {
+    var obj = {
+      say: function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(global)
         }
         return _say.bind(obj)
       }()
-      obj.say()
-    })
+    }
+    obj.say()
   })
+
+  it('pointing to a new object', function () {
+    var obj ={};
+    var temp = obj; 
+    obj = {
+      say: function () {
+        function _say() {
+          // this 是什么？想想为什么？
+          this.should.equal(temp)
+        }
+        return _say.bind(obj)
+      }()
+    }
+    obj.say()
+  })
+
+  it('bind normal', function () {
+    var obj = {}
+    obj.say = function () {
+      function _say() {
+        // this 是什么？想想为什么？
+        this.should.equal(obj)
+      }
+      return _say.bind(obj)
+    }()
+    obj.say()
+  }
+
+  )
 })
